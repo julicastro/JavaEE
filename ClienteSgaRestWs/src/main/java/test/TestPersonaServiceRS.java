47d54eee44b24b485c50030429564c9e35965fde
julicastro <castroejulian@gmail.com>
2021-03-04 18:30:53 -0300

    Agregando seguridad JavaEE

--- ClienteSgaRestWs/src/main/java/test/TestPersonaServiceRS.java
+++ ClienteSgaRestWs/src/main/java/test/TestPersonaServiceRS.java
@@ -4,78 +4,84 @@ import domain.Persona;
 import java.util.List;
 import javax.ws.rs.client.*;
 import javax.ws.rs.core.*;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 
 public class TestPersonaServiceRS {
-    
-    //variable a utilizar
+
+    //Variables que vamos a utilizar
     private static final String URL_BASE = "http://localhost:8080/sga-jee-web/webservice";
     private static Client cliente;
     private static WebTarget webTarget;
     private static Persona persona;
-    private static List <Persona> personas;
+    private static List<Persona> personas;
     private static Invocation.Builder invocationBuilder;
     private static Response response;
-    
+
     public static void main(String[] args) {
-        
-        cliente = ClientBuilder.newClient();
-        
-        //leer una persona (metodo get)
-        webTarget = cliente.target(URL_BASE).path("/personas"); // llamada al web service
-        
-        // proporicionamos un idPersona valido
+
+        HttpAuthenticationFeature feature = HttpAuthenticationFeature.basicBuilder().nonPreemptive().credentials("admin", "admin").build();
+
+        ClientConfig clientConfig = new ClientConfig();
+        clientConfig.register(feature);
+
+        cliente = ClientBuilder.newClient(clientConfig);
+
+        //Leer una persona (metodo get)
+        webTarget = cliente.target(URL_BASE).path("/personas");
+        //Proporcionamos un idPersona valido
         persona = webTarget.path("/1").request(MediaType.APPLICATION_XML).get(Persona.class);
-        System.out.println("Persona recuperada: " + persona);
-        
-        // Leer todas las personas (metodo get con readentity de tipo list<>)
-        personas = webTarget.request(MediaType.APPLICATION_XML).get(Response.class).readEntity(new GenericType<List<Persona>>(){});
-        System.out.println("\n Personas recuperadas:");
+        System.out.println("Persona recuperada:" + persona);
+
+        //Leer todas las personas (metodo get con readEntity de tipo List<>
+        personas = webTarget.request(MediaType.APPLICATION_XML).get(Response.class).readEntity(new GenericType<List<Persona>>() {
+        });
+        System.out.println("\nPersonas recuperadas");
         imprimirPersonas(personas);
-        
+
         //Agregar una persona (metodo post)
         Persona nuevaPersona = new Persona();
-        nuevaPersona.setNombre("Emily");
-        nuevaPersona.setApellido("Stelly");
-        nuevaPersona.setEmail("esic3@mail.com");
-        nuevaPersona.setTelefono("1231321549");
-        
+        nuevaPersona.setNombre("Carlos");
+        nuevaPersona.setApellido("Miranda");
+        nuevaPersona.setEmail("cmiranda4@mail.com");
+        nuevaPersona.setTelefono("99887700");
+
         invocationBuilder = webTarget.request(MediaType.APPLICATION_XML);
         response = invocationBuilder.post(Entity.entity(nuevaPersona, MediaType.APPLICATION_XML));
         System.out.println("");
         System.out.println(response.getStatus());
-        //recuperamos la persona recien agregada para dsp modificarla y al final eliminarla
+        //Recuperamos la personas recien agregada para despues modificarla y al final eliminarla
         Persona personaRecuperada = response.readEntity(Persona.class);
-        System.out.println("Persona agregada: " + personaRecuperada);
-        
+        System.out.println("Persona agregada:" + personaRecuperada);
+
         //Modificar la persona (metodo put)
-        // persona recuperada anteriormete
+        //persona recuperada anteriormente
         Persona personaModificar = personaRecuperada;
-        personaModificar.setApellido("Peretti");
+        personaModificar.setApellido("Ramirez");
         String pathId = "/" + personaModificar.getIdPersona();
         invocationBuilder = webTarget.path(pathId).request(MediaType.APPLICATION_XML);
         response = invocationBuilder.put(Entity.entity(personaModificar, MediaType.APPLICATION_XML));
+
         System.out.println("");
-        System.out.println("response: " + response.getStatus());
-        System.out.println("Persona modificada: " + response.readEntity(Persona.class));
-        
-        //Eliminar una persona
-        //la persona recuperada anteriormete
+        System.out.println("response:" + response.getStatus());
+        System.out.println("Persona modifica:" + response.readEntity(Persona.class));
+
+        //eliminar una persona
+        //persona recuperada anteriormente
         Persona personaEliminar = personaRecuperada;
-        String pathEliminar = "/" + personaEliminar.getIdPersona();
-        invocationBuilder = webTarget.path(pathEliminar).request(MediaType.APPLICATION_XML);
+        String pathEliminarId = "/" + personaEliminar.getIdPersona();
+        invocationBuilder = webTarget.path(pathEliminarId).request(MediaType.APPLICATION_XML);
         response = invocationBuilder.delete();
         System.out.println("");
-        System.out.println("response: " + response.getStatus());
-        System.out.println("Persona Eliminada: " + personaEliminar);
-        
-        
+        System.out.println("response:" + response.getStatus());
+        System.out.println("Persona Eliminada" + personaEliminar);
+
     }
 
     private static void imprimirPersonas(List<Persona> personas) {
         for (Persona p : personas) {
-            System.out.println("Persona: " + p);
+            System.out.println("Persona:" + p);
         }
     }
-    
-    
+
 }
--- ClienteSgaWs/src/main/java/testclientews/TestPersonaServiceWs.java
+++ ClienteSgaWs/src/main/java/testclientews/TestPersonaServiceWs.java
@@ -4,23 +4,22 @@ import clientews.servicio.Persona;
 import clientews.servicio.PersonaServiceImplService;
 import clientews.servicio.PersonaServiceWs;
 import java.util.List;
+import javax.xml.ws.BindingProvider;
 
-public class TestPersonaServiceWs {
-    
+public class TestPersonaServiceWS {
     public static void main(String[] args) {
-        
         PersonaServiceWs personaService = new PersonaServiceImplService().getPersonaServiceImplPort();
         
-        System.out.println("Ejecutnado servicio de ejecutar PersonasWs");
-        List<Persona > personas = personaService.listarPersonas();
-        for (Persona persona : personas) {
-            System.out.println("persona idpersona = " + persona.getIdPersona() + ", nombre = " + persona.getNombre() + ", apellido = " + persona.getApellido() + ", email = " + persona.getEmail());
+        ((BindingProvider)personaService).getRequestContext().put(BindingProvider.USERNAME_PROPERTY, "admin");
+        ((BindingProvider)personaService).getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, "admin");
+        
+        System.out.println("ejecutando servicio listar personas ws");
+        
+        List<Persona> personas = personaService.listarPersonas();
+        for(Persona persona: personas){
+            System.out.println("Persona idPersona:" + persona.getIdPersona() + ", nombre:" + persona.getNombre() 
+            + ", apellido:" +  persona.getApellido() + ", email:" + persona.getEmail());
         }
-        System.out.println("Fin del servicio listar PersonasWs");
-        
-        
+        System.out.println("Fin servicio listar personas ws");
     }
-    
-    
-    
 }
